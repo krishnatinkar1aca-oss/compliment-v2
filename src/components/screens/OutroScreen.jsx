532defdb8c5084cb1fe0ee75847e936c2f4eb104
krishnatinkar1aca-oss <krishnatinkar1aca@gmail.com>
2026-03-19 12:51:56 +0530

    Refactor OutroScreen layout and text styling

--- src/components/screens/OutroScreen.jsx
+++ src/components/screens/OutroScreen.jsx
@@ -1,16 +1,14 @@
-
 export default function OutroScreen() {
   return (
-    <div
-      className="flex flex-col items-center justify-center p-6 text-center"
-    >
+    <div className="flex flex-col items-center justify-center p-4 text-center">
       <div className="mb-4">
         <img src="/gifs/happy.gif" alt="happy gif" className="w-40" />
       </div>
-
-      <h1 className="text-4xl md:text-5xl font-dancing text-rose-500/85 font-semibold mb-3">You're the best</h1>
+      
+      <h1 className="text-4xl md:text-5xl font-dancing text-rose-500/85 font-semibold mb-3">
+        You're the best! ✨
+      </h1>
       <p className="text-rose-400/80">Don't ever forget it</p>
-
     </div>
   );
-};
+}
